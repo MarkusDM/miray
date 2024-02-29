be67d5065a61eaa9d93882ab91db27f186b5b341
Dmitry <mazarini200@yandex.ru>
2024-02-29 16:45:16 +0500

    first commit

---
+++ src/js/dev/markusDM.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+
+
+export const rem = function (rem) {
+  if ($(window).width() > 768) {
+    return 0.005208335 * $(window).width() * rem;
+  } else {
+    return (100 / 390) * (0.1 * $(window).width()) * rem;
+  }
+};
+
+
+
+
---
+++ src/js/library/swiper.js
@@ -0,0 +1,31 @@
+import Swiper from 'swiper';
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+    const saleSwiper = document.querySelector('.sale__swiper');
+    if (saleSwiper) {
+        // Initialize Swiper inside the DOMContentLoaded event listener
+        new Swiper(saleSwiper, {
+            modules: [EffectFade, Navigation, Pagination],
+            slidesPerView: 1,
+            spaceBetween: 20,
+            effect: 'fade',
+            loop: true,
+            speed: 300,
+            mousewheel: {
+                invert: false,
+            },
+            pagination: {
+                el: '.sale__pagination-bullets',
+                clickable: true,
+                dynamicBullets: true
+            },
+        });
+    }
+});
