c1ce8921194a6a8e68b08195d87d407317805c95
vyazemskaya <vyazemskayanna@gmail.com>
2024-05-14 16:14:59 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
 
 // ---------------------------------- utils ---------------------------------
 
-import * as utils from './utils/utils.js';
+import * as utils from "./utils/utils.js";
 
 // hamburger menu
 utils.menuInit();
@@ -13,40 +13,40 @@ utils.showmore();
 // ------------------------------- components -------------------------------
 
 // forms
-// import './utils/forms';
+import "./utils/forms";
 
 // tabs
-import './utils/tabs.js';
+import "./utils/tabs.js";
 
 // accordion
-import './utils/accordion.js';
+import "./utils/accordion.js";
 
 // select
-import './utils/select.js';
+import "./utils/select.js";
 
 // modals
-import './utils/modals.js';
+import "./utils/modals.js";
 
 // modals
-import './utils/modals.js';
+import "./utils/modals.js";
 
 // ---------------------------------- libs ----------------------------------
 
 //swiper
-import './library/swiper.js';
+import "./library/swiper.js";
 
 //input-mask
-import './library/input-mask.js';
+import "./library/input-mask.js";
 
 //nouislider
-import './library/nouislider';
+import "./library/nouislider";
 
 // dynamic dom
-import './library/dd';
+import "./library/dd";
 
 // --------------------------------------------------------------------------
 
-import './dev/vzmsk1.js';
-import './dev/markusDM.js';
-import './dev/ukik0.js';
-import './dev/kie6er.js';
+import "./dev/vzmsk1.js";
+import "./dev/markusDM.js";
+import "./dev/ukik0.js";
+import "./dev/kie6er.js";
