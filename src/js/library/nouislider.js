32257f4cc4e9184cdf9780f7c03a17bc98af4398
Dmitry <mazarini200@yandex.ru>
2024-06-13 22:58:16 +0500

    update

--- src/js/library/nouislider.js
+++ src/js/library/nouislider.js
@@ -2,28 +2,57 @@ import noUiSlider from 'nouislider';
 
 export const rangeSlider = document.querySelector('.range-slider');
 document.addEventListener('DOMContentLoaded', () => {
+ 
+
+    const inputFrom = document.getElementById('from');
+    const inputTo = document.getElementById('to');
+    const elementsStart = document.querySelector('[data-value-start]');
+    const elementsEnd = document.querySelector('[data-value-end]');
+    const attributeValuesRange = []; 
+   
 
     if (rangeSlider) {
+        console.log(typeof inputFrom.dataset.fromMin);
+        console.log(typeof inputTo.dataset.toMax);
+        console.log(typeof inputFrom.dataset.startNumber);
+        console.log(typeof inputTo.dataset.endNumber);
+
+        const startValue = parseInt(inputFrom.dataset.startNumber, 10);
+        const endValue = parseInt(inputTo.dataset.endNumber, 10);
+        attributeValuesRange.push(startValue, endValue);
+
+
         noUiSlider.create(rangeSlider, {
             range: {
-                min: 20000,
-                max: 850000
+                min: Number(inputFrom.dataset.fromMin),
+                max: Number(inputTo.dataset.toMax)
             },
+            values: 0,
             step: 1,
-            start: [20000, 550000],
-            connect: true
+            start: attributeValuesRange,
+            connect: true,
+            format: {
+                to: function (value) {
+                    return Math.round(value).toLocaleString('ru-RU') + '₽';
+                },
+                from: function (value) {
+                    return value.replace(' ₽', '');
+                }
+            }
         });
 
-        const inputFrom = document.getElementById('from');
-        const inputTo = document.getElementById('to');
+      
 
         const inputs = [inputFrom, inputTo];
 
         rangeSlider.noUiSlider.on('update', (values, handle) => {
-            inputs[handle].value = Math.round(values[handle]);
+            inputs[handle].value = values[handle];
         });
 
         inputs.forEach((input, index) => {
+            input.addEventListener('click', () => {
+                input.value = "";
+            })
             input.addEventListener('change', (event) => {
                 const range = [null, null];
                 range[index] = event.currentTarget.value;
@@ -32,4 +61,4 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-})
+});
